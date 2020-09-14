7564d84102aa49e7080ef8e8e433a4c4218ec606
Benja <benjamin.bravo@usach.cl>
2020-09-14 13:12:50 -0300

    Main lleva a vistas según botón

--- app/src/main/java/com/example/compraapp/MantencionDeProductos.java
+++ app/src/main/java/com/example/compraapp/MantencionDeProductos.java
@@ -2,7 +2,10 @@ package com.example.compraapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MantencionDeProductos extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MantencionDeProductos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mantencion_de_productos);
+
+        Button btnRegistrar = findViewById(R.id.btn_RegistrarProducto);
+        Button btnEditarEliminar = findViewById(R.id.btn_EditarEliminarProducto);
+
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MantencionDeProductos.this, RegistrarProducto.class);
+                startActivity(i);
+            }
+        });
+
+        btnEditarEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MantencionDeProductos.this, EditarEliminarProducto.class);
+                startActivity(i);
+            }
+        });
     }
 }
