73966aab1b2e6b081d2c919d0e5095f148df80b2
Vengerstein <hernan.diaz.f@usach.cl>
2020-09-21 15:59:05 -0300

    Ventana Registrar Usuario priri ok :d

---
+++ app/src/main/java/com/example/compraapp/RegistrarUsuario.java
@@ -0,0 +1,14 @@
+package com.example.compraapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrarUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_usuario);
+    }
+}
